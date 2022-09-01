99f19817bd8d10fa6fd7a0d0b45b434e7a2af979
Alun Price <alun.price@ecoonline.com>
2022-09-01 13:04:34 +0100

    First Practice Assignment Done

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,17 @@
+let js = 'amazing';
+if (js === 'amazing') alert('Javascript is FUN!!');
+
+console.log(40 + 8 + 23 - 10);
+
+console.log('Alun');
+console.log(23);
+
+let firstName = "Alun";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "scientist";
+let mySecondJob = "programmer";
+
+console.log(myFirstJob);
